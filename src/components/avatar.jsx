4942ecc60bf137ab3a0a3427aa93d7e29617c4f2
Rafael Furlan <rafael.furlan@example.com>
2025-08-08 23:06:22 +0200

    mobile version optimization

--- src/components/avatar.jsx
+++ src/components/avatar.jsx
@@ -2,8 +2,8 @@ import React from 'react';
 
 function Avatar() {
   return (
-    <div className='p-[5px] rounded-full bg-gray-600/20 backdrop-blur-md hover:bg-gray-600/40 transition-all duration-300' data-cursor="hidden">
-    <div className="min-w-[60px] min-h-[60px] bg-[url('/images/portrait/fractal-portrait.jpg')] bg-cover bg-center rounded-full"></div>
+    <div className='p-[4px] rounded-full bg-gray-600/20 backdrop-blur-md hover:bg-gray-600/40 transition-all duration-300' data-cursor="hidden">
+    <div className="min-w-[46px] min-h-[46px] max-w-[46px] max-h-[46px] sm:min-w-[60px] sm:min-h-[60px] sm:max-w-[60px] sm:max-h-[60px] bg-[url('/images/portrait/fractal-portrait.jpg')] bg-cover bg-center rounded-full"></div>
     </div>
   );
 }
--- src/pages/work.jsx
+++ src/pages/work.jsx
@@ -11,7 +11,7 @@ function WorkPage() {
     // Detecta se é dispositivo touch
     setIsTouchDevice('ontouchstart' in window || navigator.maxTouchPoints > 0);
   }, []);
-  
+
   const [scrollY, setScrollY] = useState(0);
 
   useEffect(() => {
@@ -38,29 +38,27 @@ function WorkPage() {
       <div className='w-full min-h-screen flex flex-col overflow-visible cursor-none bg-gray-50 overflow-hidden'>
       <TopBar/>
       
-      <div id="main" className='flex flex-col w-full flex-1 gap-[50px] overflow-hidden pt-[200px] pb-0 pr-8 pl-8 sm:pl-16 sm:pr-16 sm:pb-16 ' data-cursor="scroll">
+      <div id="main" className='flex flex-col w-full flex-1 gap-[25px] overflow-hidden pt-[120px] pb-0 pr-6 pl-6 sm:pt-[200px] sm:pl-16 sm:pr-16 sm:pb-16 ' data-cursor="scroll">
         <div>
-            <p className='text-gray-800 font-sora font-regular text-lg max-w-[600px]'>
+            <p className='text-gray-800 font-sora font-regular text-md sm:text-lg sm:max-w-[600px]'>
             Hey, I'm Rafa! I'm a digital product designer and frontend enthusiast.
             I have 150+ years of experience in design. Working across web and mobile design, user experience projects and design systems has shaped my belief in holistic design - where designers own the entire journey from strategy to implementation.
             </p>
         </div>
         <div className='whitespace-nowrap'>
-          <div 
-            className='flex'
-            style={{ 
+          <div className='flex' style={{ 
               transform: `translateX(${translateX}%)`,
               transition: 'transform 0.1s linear'
             }}
           >
-            <h1 className='text-gray-800 font-sora text-[150px] font-medium tracking-tighter whitespace-nowrap'>Product Design Engineer</h1>
+            <h1 className='text-gray-800 font-sora text-[100px] sm:text-[150px] font-medium tracking-tighter whitespace-nowrap'>Product Design Engineer</h1>
           </div>
         </div>
         
       </div>
       
      
-      <div id="grid" className='grid gri-cols-1 pl-8 pr-8 pb-16 gap-4 sm:grid-cols-2 sm:pl-16 sm:pr-16 sm:pb-16 sm:gap-4' data-cursor="view">
+      <div id="grid" className='grid gri-cols-1 pt-8 pl-6 pr-6 pb-16 gap-4 sm:grid-cols-2 sm:pl-16 sm:pr-16 sm:pb-16 sm:gap-4' data-cursor="view">
         <div className='bg-[url(/images/project-placeholders/img-8.png)] w-full aspect-square flex items-center justify-center' ></div>
         <div className='bg-[url(/images/project-placeholders/img-6.png)] w-full aspect-square flex items-center justify-center' ></div>
         <div className='bg-[url(/images/project-placeholders/img-7.png)] w-full aspect-square flex items-center justify-center' ></div>
@@ -68,24 +66,26 @@ function WorkPage() {
       </div>
       
      
-      <div  id="bottom" className='flex flex-row w-full flex items-center justify-start pl-16 pr-16 pt-16 pb-[150px] gap-[100px]' data-cursor="top" onClick={() => window.scrollTo({ top: 0, behavior: 'smooth' })}>
-      
-      
-        <div className='flex flex-col gap-2 p-2'>
-          <h3 className='text-gray-500 text-lg'>Based</h3>
-          <h2 className='text-gray-700 text-xl font-medium'>Barcelona, Spain</h2>
-        </div>
-        <div className='flex flex-col gap-2 p-2'>
-          <h3 className='text-gray-500 text-lg'>Company</h3>
-          <h2 className='text-gray-700 text-xl font-medium'>Superfiliate Inc.</h2>
-        </div>
-        <div className='flex flex-col gap-2 p-2'>
-          <h3 className='text-gray-500 text-lg'>Contact</h3>
-          <h2 className='text-gray-700 text-xl font-medium'>rafaelfurlan@gmail.com</h2>
-        </div>
+       <div id="bottom" className='flex flex-col sm:flex-row w-full items-start justify-start pl-6 pr-6 pt-8 pb-[50px] gap-[20px] sm"gap-[40px] sm:pl-16 sm:pr-16 sm:pt-16 sm:pb-[150px] sm:gap-[100px]' data-cursor="top" onClick={() => window.scrollTo({ top: 0, behavior: 'smooth' })}>
+    
+          <div className='flex flex-col gap-2 p-2 items-start'>
+            <h3 className='text-gray-500 text-lg'>Based</h3>
+            <h2 className='text-gray-700 text-xl font-medium'>Barcelona, Spain</h2>
+          </div>
+          
+          <div className='flex flex-col gap-2 p-2 items-start'>
+            <h3 className='text-gray-500 text-lg'>Company</h3>
+            <h2 className='text-gray-700 text-xl font-medium'>Superfiliate Inc.</h2>
+          </div>
+          
+          <div className='flex flex-col gap-2 p-2 items-start'>
+            <h3 className='text-gray-500 text-lg'>Contact</h3>
+            <h2 className='text-gray-700 text-xl font-medium'>rafaelfurlan@gmail.com</h2>
+          </div>
+    
       </div>
-      
-    </div>
+        
+      </div>
     </>
   );
 }
