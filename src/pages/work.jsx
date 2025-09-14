e28da16f8fa3311293b041c43267c86e898503b4
Rafael Furlan <rafael.furlan@example.com>
2025-09-14 15:48:06 +0200

    Fix image paths in work.jsx grid

--- src/pages/work.jsx
+++ src/pages/work.jsx
@@ -28,31 +28,35 @@ function WorkPage() {
 
     {!isTouchDevice && <CustomCursor />}
 
-      <div className='w-full min-h-screen flex flex-col cursor-none bg-gray-50'>
+      <div className='w-full min-h-screen flex flex-col cursor-none bg-gray-50 '>
+      <TopBar/>
+      
+      <div id="main" className='flex flex-col w-full flex-1 gap-[25px] pt-[160px] pb-0 pr-6 pl-6 sm:pt-[180px] sm:pl-16 sm:pr-16 sm:pb-8 cursor-none ' data-cursor="scroll">
+        <div className='flex fle-row w-full sm:max-w-[600px]'>
+            <p className='text-gray-800 font-sora font-regular  text-lg/9'>
+            Hey, <strong>I'm Rafa!</strong><br></br> I'm a digital product designer and frontend engineering enthusiast
+            with 7+ years of design experience. Working across web and mobile design,
+            UX projects, and design systems has shaped my belief in holistic design
+            —where designers own the entire journey from strategy to implementation.
+            </p>
+        </div>
+      </div>
 
-        <TopBar/>
-        
-        <div id="heading" className='flex flex-col w-full pt-9 pb-8 sm:pt-44 sm:pb-28 cursor-none' data-cursor="scroll">
+        <div id="heading" className='flex flex-col w-full pt-9 pb-8 sm:pt-12 pb-16 cursor-none' data-cursor="scroll">
 
-          <div className='flex flex-row justify-center gap-4'>
-              <Marquee autoFill={true}>
-                <div className='flex flex-row justify-center ml-8 mr-8 h-[112px]'>
-                    <h1 className='text-gray-800 font-sora text-[120px] sm:text-8xl font-medium tracking-tighter'>Staff Product Designer</h1>
-                </div>
-              </Marquee>
-          </div>
-
-          <div id="main" className='flex flex-col w-full flex-1 gap-[25px] pt-[160px] pb-0 pr-6 pl-6 sm:pt-20  sm:pr-12 sm:pl-12 cursor-none ' data-cursor="scroll">
-          <div className='flex fle-row w-full sm:max-w-[820px]'>
-              <p className='text-gray-800 font-sora font-regular text-xl leading-[2.0] text-justify'>
-              Hey, <strong>I'm Rafa!</strong><br></br>
-              I'm a Digital product designer and Frontend enthusiast. I have 7+ years 
-              of experience in design field. Working across web and mobile design, user 
-              experience projects and design systems has shaped my belief in holistic design 
-              - where designers own the entire journey from strategy to implementation.
-              </p>
-          </div>
-
+        <div className='flex flex-row justify-center gap-16 items-baseline'>
+            <Marquee autoFill={true}>
+              <div className='flex flex-row justify-center ml-8 mr-8'>
+                  <h1 className='text-gray-800 font-sora text-[120px] sm:text-[150px] font-medium tracking-tighter'>Staff</h1>
+              </div>
+              <div className='flex flex-row justify-center ml-8 mr-8'>
+                  <h1 className='text-gray-800 font-sora text-[120px] sm:text-[150px] font-medium tracking-tighter'>Product</h1>
+              </div>
+              <div className='flex flex-row justify-center ml-8 mr-8'>
+                  <h1 className='text-gray-800 font-sora text-[120px] sm:text-[150px] font-medium tracking-tighter'>Designer</h1>
+              </div>
+             
+            </Marquee>
         </div>
 
       </div>
@@ -61,19 +65,19 @@ function WorkPage() {
       <div id="grid" className='grid gri-cols-1 pt-8 pl-6 pr-6 gap-4 sm:pt-0 sm:grid-cols-2 sm:pr-12 sm:pl-12 sm:gap-4' data-cursor="view">
 
         <Link to="/project-pleez" className='cursor-none'>
-        <div className='bg-[url(public/images/project-placeholders/pleez-cover.jpg)] bg-cover w-full aspect-square flex items-center justify-center rounded-lg'></div>
+        <div className='bg-[url(/images/project-placeholders/pleez-cover.jpg)] bg-cover w-full aspect-square flex items-center justify-center rounded-lg'></div>
         </Link>
 
         <Link to="/project-pleez" className='cursor-none'>
-          <div className='bg-[url(public/images/project-placeholders/viplan-cover.jpg)] bg-cover w-full aspect-square flex items-center justify-center rounded-lg'></div>        
+          <div className='bg-[url(/images/project-placeholders/viplan-cover.jpg)] bg-cover w-full aspect-square flex items-center justify-center rounded-lg'></div>        
         </Link>
 
         <Link to="/project-pleez" className='cursor-none'>
-          <div className='bg-[url(public/images/project-placeholders/SF-cover.jpg)] bg-cover w-full aspect-square flex items-center justify-center rounded-lg'></div>        
+          <div className='bg-[url(/images/project-placeholders/SF-cover.jpg)] bg-cover w-full aspect-square flex items-center justify-center rounded-lg'></div>        
         </Link>
 
         <Link to="/project-pleez" className='cursor-none'>
-          <div className='bg-[url(public/images/project-placeholders/shipify-cover.jpg)] bg-cover w-full aspect-square flex items-center justify-center rounded-lg'></div>        
+          <div className='bg-[url(/images/project-placeholders/shipify-cover.jpg)] bg-cover w-full aspect-square flex items-center justify-center rounded-lg'></div>        
         </Link>
 
       </div>
