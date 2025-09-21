47820dac986b8a3e74ec9ff75cba93718207b533
Rafael Furlan <rafael.furlan@example.com>
2025-09-21 16:58:11 +0200

    Fix CSS issues: use basic.css without Tailwind processing to resolve image errors

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import App from './app.jsx' 
-import '../Styles/global.css'
+import '../Styles/basic.css'
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
